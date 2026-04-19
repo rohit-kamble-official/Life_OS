e9bf733b2a4af83fd75415aa4b81e6524113499c
Rohit Kamble <itsrohitrk.official@gmail.com>
2026-04-19 23:26:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,25 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:5000', changeOrigin: true }
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
+  },
+
+  // ✅ REQUIRED for Render
+  preview: {
+    host: true,
+    port: process.env.PORT,
+    allowedHosts: 'all'
   }
 })
